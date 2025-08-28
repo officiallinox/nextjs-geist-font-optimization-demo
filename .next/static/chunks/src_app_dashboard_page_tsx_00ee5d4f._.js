4b3e5a71fd89efe78d8bb7e936bdeec3cc5d444d
officiallinox <pillylinox04@gmail.com>
2025-08-28 10:42:25 +0300

    Add .next/static/chunks/src_app_dashboard_page_tsx_00ee5d4f._.js 1756366943073

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_00ee5d4f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_c5e50cc9._.js"
+],
+    source: "dynamic"
+});
